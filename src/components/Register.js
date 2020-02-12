7cbdabe770e1c780bb7b5a745116e0e1a6c82847
Jason Aviles <sandman2k18@outlook.com>
2020-02-12 16:43:38 -0500

    fix error handle

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,124 +12,103 @@ import {
 import { register, handleChange } from "./apis/apis.js";
 
 class Register extends Component {
-  state = 
-   
- {username: " ",
-    password: "",
-    email: " ",
-    err:'',show:false}
-  
-componentDidMount(){
+  state = { username: " ", password: "", email: " ", err: "", show: false };
 
-
-  this.register = async user => {
-    try {
-      const res = await axios.post(
-        "https://foodappapisql.herokuapp.com/auth/register",
-        user
-      );
-      console.log(res,'res')
-      if(res.data.message){
-       return this.setState({err:res.data.message})
+  componentDidMount() {
+    this.register = async user => {
+      try {
+        const res = await axios.post(
+          "https://foodappapisql.herokuapp.com/auth/register",
+          user
+        );
+        console.log(res, "res");
+        if (res.data.message) {
+          return this.setState({ err: res.data.message });
+        }
+        this.props.history.push("/login");
+        return res.data;
+      } catch (err) {
+        throw new Error(err);
       }
-      this.props.history.push("/login");
-     return res.data;
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
-
-}
-// register = async user => {
-//     try {
-//       const res = await axios.post(
-//         "https://foodappapisql.herokuapp.com/auth/register",
-//         user
-//       );
-//       console.log(res,'res')
-//       if(res.data.message){
-//        return this.setState({err:res.data.message})
-//       }
-//      return res.data;
-//     } catch (err) {
-//       throw new Error(err);
-//     }
-//   };
-  
+    };
+  }
+  // register = async user => {
+  //     try {
+  //       const res = await axios.post(
+  //         "https://foodappapisql.herokuapp.com/auth/register",
+  //         user
+  //       );
+  //       console.log(res,'res')
+  //       if(res.data.message){
+  //        return this.setState({err:res.data.message})
+  //       }
+  //      return res.data;
+  //     } catch (err) {
+  //       throw new Error(err);
+  //     }
+  //   };
 
   handleChange = e => {
-    if(!e.target.value){
+    if (!e.target.value) {
       this.setState({ [e.target.name]: "" });
     }
     this.setState({ [e.target.name]: e.target.value });
   };
   handleSubmit = e => {
     e.preventDefault();
-    let newReg={
-      username:this.state.username,
-      email:this.state.email,
-      password:this.state.password
-    }
-    if(!newReg.username||!newReg.password ||!newReg.email){
-      return
+    let newReg = {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    };
+    if (!newReg.username || !newReg.password || !newReg.email) {
+      return;
     }
     this.register(newReg);
-   
   };
 
   render() {
-
-    const flickShow =  () => {
-      return this.setState(
-  {
-          show: !this.state.show}
-      );
+    const flickShow = () => {
+      return this.setState({
+        show: !this.state.show
+      });
     };
     return (
       <div>
-      
         <Container>
-
-        
-          <Form onSubmit={this.handleSubmit} data-testid="form">{!this.state.username ||this.state.username=== " " ||   !this.state.email ||  this.state.email=== " " ||!this.state.password  ||  this.state.password === "" ?   <i class="exclamation triangle icon" style={{fontSize:".7rem",color: "#f22626",
-    position:" relative",    bottom: "-45px",
-    left: "85px"}}>required</i> : "" }
+          <Form onSubmit={this.handleSubmit} data-testid="form">
+            {!this.state.username ||
+            this.state.username === " " ||
+            !this.state.email ||
+            this.state.email === " " ||
+            !this.state.password ||
+            this.state.password === "" ? (
+              <i
+                class="exclamation triangle icon"
+                style={{
+                  fontSize: ".7rem",
+                  color: "#f22626",
+                  position: " relative",
+                  bottom: "-45px",
+                  left: "85px"
+                }}
+              >
+                required
+              </i>
+            ) : (
+              ""
+            )}
             <H1>Register</H1>
-
-           username: <Input
-           placeholder="username"
+            username:{" "}
+            <Input
+              placeholder="username"
               data-testid="username"
               name="username"
               value={this.state.username}
               onChange={this.handleChange}
             ></Input>
-       {!this.state.username && (
-              <h4
-                style={{
-                  background: "#d53f3fbd",
-                  width: "200px",
-                  padding: "5px 5px",margin:"0px",marginBottom:'10px'
-                }}
-              >
-               username field is empty
-              </h4>
-            )}
-            password:<Input
-            placeholder="password"
-              data-testid="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            ></Input>
-    
-            email:<Input
-            placeholder='email'
-              data-testid="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            ></Input>
-           {!this.state.show ? (
+        
+           password: {!this.state.show ? (
               <Input
                 type="password"
                 name="password"
@@ -147,7 +126,9 @@ componentDidMount(){
               />
             )}
             <span
-              onClick={() => {flickShow()}}
+              onClick={() => {
+                flickShow();
+              }}
               style={{
                 background: "#b6b2b2",
                 position: " relative",
@@ -161,12 +142,31 @@ componentDidMount(){
             >
               show
             </span>
+            email:
+            <Input
+              placeholder="email"
+              data-testid="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            ></Input>
             <Button data-testid="btn">signUp</Button>
-          
-         { this.state.err &&  <h4  style={{background:'rgba(213, 63, 63, 0.04)',width:'200px',padding:"5px 5px"}}>
-         
-         <i class="exclamation triangle icon" style={{fontSize:".9rem",color: "#f22626"}}>{this.state.error}</i>
-         </h4>}
+            {this.state.err && (
+              <h4
+                style={{
+                  background: "rgba(213, 63, 63, 0.04)",
+                  width: "200px",
+                  padding: "5px 5px"
+                }}
+              >
+                <i
+                  class="exclamation triangle icon"
+                  style={{ fontSize: ".9rem", color: "#f22626" }}
+                >
+                  {this.state.error}
+                </i>
+              </h4>
+            )}
           </Form>
 
           <ImgContainer>
